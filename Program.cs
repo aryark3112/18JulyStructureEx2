f4ded43ef34a9fe023dd1cc2801a24b8e2a3c499
aryark <aryark3112@gmail.com>
2023-07-18 12:02:40 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,74 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace _18JulyCustomException
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//namespace _18JulyCustomException
+//{
+//public class CustomException:Exception
+//{
+//    public CustomException(string message):base(message)
+//    {
+
+//    }
+//}
+
+//Struct Ex
+//public struct Student
+//{
+//    int id;
+//    string name;
+//    public Student(int _id,string _name)
+//    {
+//        //this.id = id;
+//        //this.name = name;
+//        id = _id;
+//        name = _name;
+//    }
+//    public void Display()
+//    {
+//        Console.WriteLine($"Student ID {id} \t Student Name {name}" );
+//    }
+//}
+//internal class Program
+//{
+//Enumeration(Enum) Example
+//public enum DayofWeek
+//{
+//    Monday=1, Tuesday, Wednesday, Thursday, Friday, Saturday
+//}
+//static void Main(string[] args)
+//{
+//try {
+//    int userAge;
+//    Console.WriteLine("Enter your age");
+//    userAge = int.Parse(Console.ReadLine());
+//    if(userAge<0)
+//    {
+//        throw new CustomException("Age must be positive number, please enter positive number only");
+//    }
+//    else if (userAge>=18)
+//    {
+//        Console.WriteLine("You are eligible for voting");
+//    }
+//    else { Console.WriteLine("You are not eligible for voting, You have to wait "); }
+//}
+//catch (CustomException ce) { Console.WriteLine("Custom Error!!!"+ce.Message); }
+//catch(Exception ex) { Console.WriteLine("Error!!!"+ex.Message); }
+//finally { Console.WriteLine("***Bye Bye***"); }
+//Console.ReadKey();
+
+//DayofWeek today = DayofWeek.Tuesday;
+//Console.WriteLine("Day: "+today);
+//Console.WriteLine("Day Number "+(int)today);
+//Console.ReadKey();
+
+//Structure Example
+//            Student student = new Student(12, "Raj");
+//            student.Display();
+//            Student student1 = new Student(13, "Ravi");
+//            student1.Display();
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+//Another Structure Example
+using System;
+namespace StructExample
 {
-    //public class CustomException:Exception
-    //{
-    //    public CustomException(string message):base(message)
-    //    {
-
-    //    }
-    //}
-
-    //Struct Ex
-    public struct Student
+    public struct Rectangle
     {
-        int id;
-        string name;
-        public Student(int _id,string _name)
+        public int width;
+        public int height;
+        
+        public void Area()
         {
-            //this.id = id;
-            //this.name = name;
-            id = _id;
-            name = _name;
+            int area = height * width;
+            Console.WriteLine($"Area of Rectangle {height}*{width}={area}");
         }
-        public void Display()
-        {
-            Console.WriteLine($"Student ID {id} \t Student Name {name}" );
-        }
+
     }
-    internal class Program
+    public class MyClass
     {
-        //Enumeration(Enum) Example
-        //public enum DayofWeek
-        //{
-        //    Monday=1, Tuesday, Wednesday, Thursday, Friday, Saturday
-        //}
-        static void Main(string[] args)
+        public static void Main()
         {
-            //try {
-            //    int userAge;
-            //    Console.WriteLine("Enter your age");
-            //    userAge = int.Parse(Console.ReadLine());
-            //    if(userAge<0)
-            //    {
-            //        throw new CustomException("Age must be positive number, please enter positive number only");
-            //    }
-            //    else if (userAge>=18)
-            //    {
-            //        Console.WriteLine("You are eligible for voting");
-            //    }
-            //    else { Console.WriteLine("You are not eligible for voting, You have to wait "); }
-            //}
-            //catch (CustomException ce) { Console.WriteLine("Custom Error!!!"+ce.Message); }
-            //catch(Exception ex) { Console.WriteLine("Error!!!"+ex.Message); }
-            //finally { Console.WriteLine("***Bye Bye***"); }
-            //Console.ReadKey();
-
-            //DayofWeek today = DayofWeek.Tuesday;
-            //Console.WriteLine("Day: "+today);
-            //Console.WriteLine("Day Number "+(int)today);
-            //Console.ReadKey();
-
-            //Structure Example
-            Student student = new Student(12, "Raj");
-            student.Display();
-            Student student1 = new Student(13, "Ravi");
-            student1.Display();
+            Rectangle rect;
+            Console.WriteLine("enter Height");
+            rect.height = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter Width");
+            rect.width = int.Parse(Console.ReadLine());
+            rect.Area();
             Console.ReadKey();
+        
         }
     }
 }
